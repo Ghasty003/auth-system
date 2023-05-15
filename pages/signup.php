41710326c088fe6b4d77ef62667eae2570731c62
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-05-15 15:50:26 -0300

    setup signup page & protected index page

---
+++ pages/signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        input {
+            margin: 5px 0;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Weclome.</h1>
+
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="username"> <br>
+        <input type="email" name="email" placeholder="email"> <br>
+        <input type="text" name="password" placeholder="password"> <br>
+        <button name="signup-submit">submit</button>
+    </form>
+</body>
+
+</html>
